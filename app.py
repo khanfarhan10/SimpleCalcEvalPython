cb8f1da83c5b4a0678001c1913da3d3431647558
CrazyCoderz <njrfarhandasilva10@gmail.com>
2020-08-16 16:14:05 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,45 @@
+"""Helllo World!
+
+Python program using Flask for a simple Calculator
+GUI using the flask module
+
+"""
+
+# import Flask Library
+
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+    # return '<h1>Hi, Welcome to this session</h1>'
+
+
+@app.route('/send', methods=['POST'])
+def send():
+    if request.method == 'POST':
+        x = request.form['x']
+        # print(x)
+        # if x is blank or
+        if x == "":
+            pass
+        else:
+            try:
+                x = int(x)
+            except:
+                return render_template('index.html', results="Non string value passed to x\n Please Provide an integer value")
+        expression = request.form['expression']
+        try:
+            results = eval(expression)
+        except:
+            return render_template('index.html', results="Oops! Your expression syntax is incorrect!")
+        return render_template('index.html', results=results)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    # maybe it runs now!
